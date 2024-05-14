fc2c5b2f631a8f9aa68b18caa1cea15f2cd0aa20
Hamza Ahmed <45544623+Hamza-101@users.noreply.github.com>
2024-05-14 17:20:18 +0500

    Update Config.py

--- src/Config.py
+++ src/Config.py
@@ -1,1 +1,13 @@
+Config = { #Find better name
+"YellowBook" : "YellowBook.json",
+"AllTransfers" : "Transfers.json",
+"NetworkDevices" : "Devices.json",
+"IncompleteTransfers" : "AbortedTransfers.json",
 
+"HeartBeat_Port" : 55000,
+"HearBeatInterval" : 30,
+"DirectoryUpdationInterval" : 30,
+
+"Log" : "Log.json"
+
+}
